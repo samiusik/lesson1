05571f3ced8fff7f25d5f058150f7f751867ab75
samiusik <sam.com007@yandex.ru>
2019-01-25 01:56:53 +0300

    weight.rb

--- weight.rb
+++ weight.rb
@@ -4,9 +4,9 @@ puts "Укажите свой рост"
 growth = gets.chomp
 puts "Укажите свой вес"
 weight = gets.chomp
-ideal = growth.to_i - 110
-	if ideal.to_i > growth.to_i
-	print "Ваш вес уже оптимальный"
-	end
-
-puts "#{name}, привет! Ваш идеальный вес составляет #{growth.to_i - 110} кг."
+ideal = growth.to_i - 110.to_i
+if ideal.to_i > weight.to_i
+	puts "Ваш вес уже оптимальный"
+else 
+	puts "#{name}, привет! Ваш идеальный вес составляет #{growth.to_i - 110} кг."
+end
