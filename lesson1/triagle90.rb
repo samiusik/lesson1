47c7fc5acfc1eb68d9ef0c28a5323ef639d16818
samiusik <sam.com007@yandex.ru>
2019-01-27 15:25:28 +0300

    Finish lesson1

---
+++ lesson1/triagle90.rb
@@ -0,0 +1,31 @@
+triagle = []
+for i in 0..2
+  puts "Введите #{i + 1} - ю сторону треугольника"
+  triagle[i] = gets.chomp.to_f
+end
+
+if triagle[0] == triagle[1] or triagle[1] == triagle[2] or triagle [2] == triagle[0]
+  triagle_ab = true
+elsif triagle[0] == triagle[1] and triagle[1] == triagle[2]
+  triagle_abc = true
+end
+
+triagle.sort!
+if triagle90 = triagle[0] ** 2 + triagle[1] ** 2 == triagle[2] **2
+  triagle90 = true
+end
+
+	
+if triagle90
+  puts "Ваш треугольник прямоугольный"
+elsif triagle90 and triagle_ab
+  puts "Ваш треугольник прямоугольный и равнобедренный"
+elsif triagle_ab
+  puts "Ваш треугольник равнобедренный"
+elsif triagle_abc
+  puts "Ваш треугольник равносторонний"
+elsif triagle_ab and triagle_abc
+  puts "Ваш треугольник равнобедренный и равносторонний"	
+else
+  puts "У вас просто треугольник"
+end
---
+++ lesson1/weight.rb
@@ -0,0 +1,12 @@
+puts "Как вас зовут?"
+name = gets.chomp
+puts "Укажите свой рост"
+growth = gets.to_i
+puts "Укажите свой вес"
+weight = gets.to_i
+ideal = growth - 110
+if ideal > weight
+  puts "Ваш вес уже оптимальный"
+else 
+  puts "#{name}, привет! Ваш идеальный вес составляет #{ideal} кг."
+end
