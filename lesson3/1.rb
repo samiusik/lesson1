efb4b11ded014c2dc500b1e866a9fe48cb2efcba
samiusik <sam.com007@yandex.ru>
2019-02-10 23:57:57 +0300

    fixed

--- lesson3/1.rb
+++ lesson3/1.rb
@@ -48,14 +48,15 @@ end
 
 
 class Train
-  attr_accessor :speed, :current_station
-  attr_reader :qty_wagon, :route, :number
+  attr_accessor :speed
+  attr_reader :quantity_wagon, :route, :number, :current_station
 
-  def initialize(number, type, qty_wagon)
+  def initialize(number, type, quantity_wagon)
     @number = number
     @type = type
     @speed = 0
-    @qty_wagon = qty_wagon
+    @routs_position = 0
+    @quantity_wagon = quantity_wagon
   end
 
   def stop
@@ -63,45 +64,31 @@ class Train
   end
 
   def add_carrige
-    if speed == 0
-      @qty_wagon += 1
-    end
+    @quantity_wagon += 1 if speed == 0
   end
 
   def remove_carrige
-    if speed == 0 && qty_wagon > 0
-      @qty_wagon -= 1
-    end
+    @quantity_wagon -= 1 if speed == 0 && quantity_wagon > 0
   end
 
-  def set_routs(routs)
-    @routs = routs
+  def set_routs(rout)
+    @routs = rout
     @current_station = @routs.stations.first
     @current_station.get_train(self)
   end
 
   def next_station
-    station_index = @routs.stations.index(@current_station)
-    if station_index >= @routs.stations.size - 1
-      puts "end of routs"
-    else
-      puts "next station is - #{@routs.stations[station_index + 1].name}"
-    end
+    @routs.stations[@routs_position + 1] unless @routs_position ==  @routs.stations.size - 1
   end
 
-  def previous_station
-    station_index = @routs.stations.index(@current_station)
-    if station_index <= 0 
-      puts "begin of routs - #{@current_station.name}"
-    else
-      puts "previous station is - #{@routs.stations[station_index - 1].name}"
-    end
+  def prev_station
+    @routs.stations[@routs_position - 1] unless @routs_position.zero?
   end
 
   def move_forward
     station_index = @routs.stations.index(@current_station)
     if station_index >= @routs.stations.size - 1
-      puts "end of routs"
+      puts "end of rout"
     else
       @current_station = @routs.stations[station_index + 1]
       @current_station.get_train(self)
@@ -111,7 +98,7 @@ class Train
   def move_back
     station_index = @routs.stations.index(@current_station)
     if station_index <= 0
-      puts "begin of routs"
+      puts "begin of rout"
     else
       @current_station = @routs.stations[station_index - 1]
       @current_station.get_train(self)
